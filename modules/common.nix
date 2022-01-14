e9a81f47b115bdf166946b05477b28c52c22e7c2
Vaseeharan Thirukumaran <vasyharan@gmail.com>
2022-01-14 18:32:57 -0500

    Add ripgreprc config file

--- modules/common.nix
+++ modules/common.nix
@@ -57,6 +57,10 @@
           source = ../dotfiles/p10k.zsh;
           target = ".p10k.zsh";
         };
+        ripgreprc = {
+          source = ../dotfiles/ripgreprc;
+          target = ".ripgreprc";
+        };
       };
       packages = with pkgs; [
         (nerdfonts.override { fonts = [ "FiraCode" "Hack" "InconsolataGo" ]; })
