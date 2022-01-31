48012e4e5694c52b74e6391d56923f645455731e
Vaseeharan Thirukumaran <vasyharan@gmail.com>
2022-01-31 16:47:17 -0500

    nvim tree tweaks

--- modules/home/vim/ui/nvim-tree.lua
+++ modules/home/vim/ui/nvim-tree.lua
@@ -1,6 +1,11 @@
 local nvimtree = require('nvim-tree')
 nvimtree.setup {
-  update_focused_file = {
-    enable = true,
+  disable_netrw = false,
+  view = {
+    auto_resize = true,
+  },
+  filters = {
+    dotfiles = false,
+    custom = { ".git" },
   },
 }
