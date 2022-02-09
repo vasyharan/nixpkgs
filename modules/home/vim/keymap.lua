1f3e180b5996d92c61dd441d80ab6cfe767eac34
Vaseeharan Thirukumaran <vasyharan@gmail.com>
2022-02-09 10:46:09 -0500

    Add keymaps for DAP UI

--- modules/home/vim/keymap.lua
+++ modules/home/vim/keymap.lua
@@ -48,6 +48,8 @@ keymap { 'n', '<space>dD', "<cmd>lua require('dap').run_last()<cr>" }
 keymap { 'n', '<space>db', "<cmd>lua require('dap').toggle_breakpoint()<cr>" }
 keymap { 'n', '<space>dB', "<cmd>lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>" }
 keymap { 'n', '<space>dr', "<cmd>lua require('dap').repl.open()<cr>" }
+keymap { 'n', '<space>du', "<cmd>lua require('dapui').open()<cr>" }
+keymap { 'n', '<space>dU', "<cmd>lua require('dapui').close()<cr>" }
 
 keymap { 'n', '<F4>', "<cmd>lua require('dap').toggle_breakpoint()<cr>" }
 keymap { 'n', '<F5>', "<cmd>lua require('dap').continue()<cr>" }
