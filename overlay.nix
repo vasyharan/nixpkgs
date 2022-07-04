7a5d494b08648c42b532ebba98f19bdd509e69ce
Vaseeharan Thirukumaran <vasyharan@gmail.com>
2022-07-04 01:11:39 -0400

    remove unused overlay

--- overlay.nix
+++ overlay.nix
@@ -4,18 +4,6 @@ let
 in
 {
   vimPlugins = vimPlugins // {
-    vim-solarized8 = vimUtils.buildVimPluginFrom2Nix rec {
-      pname = "vim-solarized8";
-      version = "1.2.0";
-      src = fetchFromGitHub {
-        owner = "lifepillar";
-        repo = "vim-solarized8";
-        rev = "v${version}";
-        sha256 = "sha256-LDH3VJ5l2FF+e0hjxlnwvdw8G2+CXf9n3FpGIIOqac0=";
-      };
-      meta.homepage = "https://github.com/lifepillar/vim-solarized8";
-    };
-
     vim-snazzy = vimUtils.buildVimPluginFrom2Nix rec {
       pname = "vim-snazzy";
       version = "d979964b4dc0d6860f0803696c348c5a912afb9e";
