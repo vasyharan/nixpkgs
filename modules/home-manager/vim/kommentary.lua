0aa6afa217b45c0e534b61e33038da4ac0cbf316
Thirukumaran Vaseeharan <vasyharan@gmail.com>
2024-05-03 13:19:07 -0700

    fixup nix comment string

--- modules/home-manager/vim/kommentary.lua
+++ modules/home-manager/vim/kommentary.lua
@@ -1,2 +1,3 @@
 local config = require('kommentary.config')
 config.configure_language("default", { prefer_single_line_comments = true })
+config.configure_language("nix", { single_line_comment_string = "#" })
