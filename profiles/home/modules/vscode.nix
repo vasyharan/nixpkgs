0ae54d593e82bef93d8158de9df3dff39a98e147
Thirukumaran Vaseeharan <vasyharan@gmail.com>
2024-05-16 22:53:11 -0400

    enable mutableExtensionsDir

--- profiles/home/modules/vscode.nix
+++ profiles/home/modules/vscode.nix
@@ -1,18 +1,18 @@
 { pkgs, ... }: {
   programs.vscode = {
     enable = true;
-    mutableExtensionsDir = false;
+    mutableExtensionsDir = true;
     extensions = with pkgs.vscode-extensions; [
       mkhl.direnv
       vscodevim.vim
       golang.go
       bbenoist.nix
       rust-lang.rust-analyzer
-      ms-vscode.cpptools
+      # ms-vscode.cpptools
     ];
     userSettings = {
       "workbench.colorTheme" = "Solarized Dark";
-      "editor.fontFamily" = "'Inconsolata Nerd Font Mono', monospace";
+      "editor.fontFamily" = "'SauceCodePro Nerd Font Mono', monospace";
       "editor.fontSize" = 13;
       "terminal.integrated.fontSize" = 13;
       "editor.minimap.enabled" = false;
